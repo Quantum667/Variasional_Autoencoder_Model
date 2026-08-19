1c13f4d65616b4bc3cfdc0be6f0b15f663ddfc12
Egor Potylitsyn <egorp170200@gmail.com>
2026-08-19 22:47:37 +0300

    Some fix model

--- train.py
+++ train.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    train_load = data_loader()
+    
     model = Model().to(device)
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
