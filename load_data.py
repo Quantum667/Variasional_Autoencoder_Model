02ae2bd228c777ae75fe465f3fd9f2e5c0b0f117
Egor Potylitsyn <egorp170200@gmail.com>
2026-08-21 10:03:38 +0300

    Rebuild a model

--- load_data.py
+++ load_data.py
@@ -1,16 +1,21 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
 from config import BATCH_SIZE, NUM_WORKERS
 
 def data_loader(batch_size=BATCH_SIZE, num_workers=NUM_WORKERS):
 
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
 
-    train_dataset = datasets.FashionMNIST(
+    train_dataset = datasets.CIFAR10(
         root="data",
         train=True,
         download=True,
-        transform=ToTensor()
+        transform=transform_train
     )
 
     train_load = DataLoader(
--- model.py
+++ model.py
@@ -7,20 +7,23 @@ class Model(nn.Module):
     def __init__(self, dim=16):
         super().__init__()
 
-        self.encode1 = nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1)
+        self.encode1 = nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1)
         self.encode2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
+        self.encode3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
 
-        self.f_mu = nn.Linear(64*7*7, dim)
-        self.f_logvar = nn.Linear(64*7*7, dim)
+        self.f_mu = nn.Linear(128*4*4, dim)
+        self.f_logvar = nn.Linear(128*4*4, dim)
 
-        self.f_decode = nn.Linear(dim, 64*7*7)
+        self.f_decode = nn.Linear(dim, 128*4*4)
 
-        self.decode1 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
-        self.decode2 = nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1)
+        self.decode1 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
+        self.decode2 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
+        self.decode3 = nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)
 
     def encoder(self, x):
         x = F.relu(self.encode1(x))
         x = F.relu(self.encode2(x))
+        x = F.relu(self.encode3(x))
         x = x.view(x.size(0), -1)
 
         mu = self.f_mu(x)
@@ -36,9 +39,10 @@ class Model(nn.Module):
 
     def decoder(self, z):
         x = F.relu(self.f_decode(z))
-        x = x.view(-1, 64, 7, 7)
+        x = x.view(-1, 128, 4, 4)
         x = F.relu(self.decode1(x))
-        x = torch.sigmoid(self.decode2(x))
+        x = F.relu(self.decode2(x))
+        x = torch.sigmoid(self.decode3(x))
 
         return x
 
